87069fdb9f5429ecc807318fee0f1df0b6c25add
rojoyin <rojoyin@gmail.com>
2023-07-16 12:43:25 -0500

    Create serializer component tests

---
+++ tests/test_resp.py
@@ -0,0 +1,51 @@
+import unittest
+
+
+class RESPTest(unittest.TestCase):
+    def test_serialize_string(self):
+        message = "Hello, World!"
+        actual = encode_simple_string(message)
+        expected = "+Hello, World!\r\n"
+        self.assertEquals(actual, expected)
+
+    def test_serialize_int(self):
+        message = 5
+        actual = encode_int(message)
+        expected = ":5\r\n"
+        self.assertEquals(actual, expected)
+
+    def test_serialize_array(self):
+        message = ["Hello", 42, None, "World"]
+        actual = encode_array(message)
+        expected = "*4\r\n$5\r\nHello\r\n:42\r\n$-1\r\n$5\r\nWorld\r\n"
+        self.assertEquals(actual, expected)
+
+    def test_serialize_bulk_strings(self):
+        message = "bye"
+        actual = encode_bulk_string(message)
+        expected = "$3\r\nbye\r\n"
+        self.assertEquals(actual, expected)
+
+    def test_serialize_errors(self):
+        message = "An error occurred"
+        actual = encode_error(message)
+        expected = "-An error occurred\r\n"
+        self.assertEquals(actual, expected)
+
+    def test_serialize_empty_bulk_string(self):
+        message = ""
+        actual = encode_bulk_string(message)
+        expected = "$0\r\n\r\n"
+        self.assertEquals(actual, expected)
+
+    def test_serialize_null_bulk_string_values(self):
+        message = None
+        actual = encode_bulk_string(message)
+        expected = "$-1\r\n"
+        self.assertEquals(actual, expected)
+
+    def test_serialize_null_array_values(self):
+        message = None
+        actual = encode_array(message)
+        expected = "*-1\r\n"
+        self.assertEquals(actual, expected)
