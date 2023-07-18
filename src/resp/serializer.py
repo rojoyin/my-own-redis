c9dfe61e7eb7341156affe1c7bcfee894cd9d5e4
Juan Carlos Jimenez <rojoyin@gmail.com>
2023-07-18 11:05:30 -0500

    Add type hints in serializer functions (#8)

--- src/resp/serializer.py
+++ src/resp/serializer.py
@@ -1,38 +1,38 @@
-def is_simple_string(message):
+def is_simple_string(message: str) -> bool:
     """Function to check if message is a simple string. Simple string must not contain CR(\r) or LF(\n) characters"""
     return "\r" not in message and "\n" not in message
 
 
-def encode_simple_string(message):
+def encode_simple_string(message: str) -> str:
     if not is_simple_string(message):
         raise ValueError("Not a valid simple string")
     return f"+{message}\r\n"
 
 
-def encode_int(message):
+def encode_int(message: int) -> str:
     if not isinstance(message, int):
         raise ValueError("Not a valid integer")
     return f":{message}\r\n"
 
 
-def encode_bulk_string(message):
+def encode_bulk_string(message: str | None) -> str:
     if message is None:
         return "$-1\r\n"
     return f"${len(message)}\r\n{message}\r\n"
 
 
-def encode_error(message):
+def encode_error(message: str) -> str:
     return f"-{message}\r\n"
 
 
-def encode_array(message):
+def encode_array(message: list[int | str | None]) -> str:
     if message is None:
         return "*-1\r\n"
     encoded_items = [encode_message(item) for item in message]
     return "*" + str(len(encoded_items)) + "\r\n" + "".join(encoded_items)
 
 
-def encode_message(message):
+def encode_message(message: str | int | None) -> str:
     if message is None:
         return encode_bulk_string(message)
     if isinstance(message, str):
@@ -41,7 +41,7 @@ def encode_message(message):
         return encode_int(int(message))
 
 
-def encode_message_to_resp(message):
+def encode_message_to_resp(message: str | int | None | list[int | str | None]) -> str:
     if isinstance(message, list):
         return encode_array(message)
 
