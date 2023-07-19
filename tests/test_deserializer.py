b36e35987dd4478382573949f0ca82d18ec50c6a
Juan Carlos Jimenez <rojoyin@gmail.com>
2023-07-19 13:29:31 -0500

    Add test cases to implement resp decoder (#9)

---
+++ tests/test_deserializer.py
@@ -0,0 +1,51 @@
+import unittest
+
+
+class RESPDeserializerTest(unittest.TestCase):
+    def test_decode_to_simple_string(self):
+        resp_data = "+Hello, World!\r\n"
+        expected = "Hello, World!"
+        actual = decode_to_simple_string(resp_data)
+        self.assertEquals(expected, actual)
+
+    def test_decode_to_bulk_string(self):
+        resp_data = "$3\r\nbye\r\n"
+        expected = "bye"
+        actual = decode_to_bulk_string(resp_data)
+        self.assertEquals(expected, actual)
+
+    def test_decode_to_int(self):
+        resp_data = ":5\r\n"
+        expected = 5
+        actual = decode_to_int(resp_data)
+        self.assertEquals(expected, actual)
+
+    def test_decode_to_none_array(self):
+        resp_data = "*-1\r\n"
+        expected = None
+        actual = decode_to_array(resp_data)
+        self.assertEquals(expected, actual)
+
+    def test_decode_to_none_bulk_string(self):
+        resp_data = "$-1\r\n"
+        expected = None
+        actual = decode_to_bulk_string(resp_data)
+        self.assertEquals(expected, actual)
+
+    def test_decode_to_error(self):
+        resp_data = "-An error occurred\r\n"
+        expected = "An error occurred"
+        actual = decode_to_error(resp_data)
+        self.assertEquals(expected, actual)
+
+    def test_decode_to_array(self):
+        resp_data = "*4\r\n$5\r\nHello\r\n:42\r\n$-1\r\n$5\r\nWorld\r\n"
+        expected = ["Hello", 42, None, "World"]
+        actual = decode_to_array(resp_data)
+        self.assertEquals(expected, actual)
+
+    def test_decode_resp_to_message(self):
+        resp_data = "*4\r\n$5\r\nHello\r\n:42\r\n$-1\r\n$5\r\nWorld\r\n"
+        expected = ["Hello", 42, None, "World"]
+        actual = decode_resp_to_message(resp_data)
+        self.assertEquals(expected, actual)
