1de9f32564cd48a71fdc4f81b32388d359e9acc1
Juan Carlos Jimenez <rojoyin@gmail.com>
2023-07-19 16:45:10 -0500

    Decode bulk string (#11)
    * Decode to bulk string
    
    * Decode none bulk string value

--- src/resp/deserializer.py
+++ src/resp/deserializer.py
@@ -7,3 +7,12 @@ def decode_to_simple_string(resp_data: str) -> str:
     encoded_pattern = rf"^\{EncodingHeader.SIMPLE_STRING}(.*){RESP_TRAILER}"
     match = re.search(encoded_pattern, resp_data)
     return match.group(1)
+
+
+def decode_to_bulk_string(resp_data: str) -> str | None:
+    if resp_data == "$-1\r\n":
+        return None
+
+    encoded_pattern = rf"^\{EncodingHeader.BULK_STRING}([0-9]+){RESP_TRAILER}(.*){RESP_TRAILER}"
+    match = re.search(encoded_pattern, resp_data)
+    return match.group(2)
--- tests/test_deserializer.py
+++ tests/test_deserializer.py
@@ -1,6 +1,6 @@
 import unittest
 
-from src.resp.deserializer import decode_to_simple_string
+from src.resp.deserializer import decode_to_simple_string, decode_to_bulk_string
 
 
 class RESPDeserializerTest(unittest.TestCase):
@@ -20,40 +20,40 @@ class RESPDeserializerTest(unittest.TestCase):
         resp_data = "$3\r\nbye\r\n"
         expected = "bye"
         actual = decode_to_bulk_string(resp_data)
-        self.assertEquals(expected, actual)
-
-    def test_decode_to_int(self):
-        resp_data = ":5\r\n"
-        expected = 5
-        actual = decode_to_int(resp_data)
-        self.assertEquals(expected, actual)
-
-    def test_decode_to_none_array(self):
-        resp_data = "*-1\r\n"
-        expected = None
-        actual = decode_to_array(resp_data)
-        self.assertEquals(expected, actual)
+        self.assertEqual(expected, actual)
 
     def test_decode_to_none_bulk_string(self):
         resp_data = "$-1\r\n"
         expected = None
         actual = decode_to_bulk_string(resp_data)
-        self.assertEquals(expected, actual)
+        self.assertEqual(expected, actual)
+
+    def test_decode_to_int(self):
+        resp_data = ":5\r\n"
+        expected = 5
+        actual = decode_to_int(resp_data)
+        self.assertEqual(expected, actual)
+
+    def test_decode_to_none_array(self):
+        resp_data = "*-1\r\n"
+        expected = None
+        actual = decode_to_array(resp_data)
+        self.assertEqual(expected, actual)
 
     def test_decode_to_error(self):
         resp_data = "-An error occurred\r\n"
         expected = "An error occurred"
         actual = decode_to_error(resp_data)
-        self.assertEquals(expected, actual)
+        self.assertEqual(expected, actual)
 
     def test_decode_to_array(self):
         resp_data = "*4\r\n$5\r\nHello\r\n:42\r\n$-1\r\n$5\r\nWorld\r\n"
         expected = ["Hello", 42, None, "World"]
         actual = decode_to_array(resp_data)
-        self.assertEquals(expected, actual)
+        self.assertEqual(expected, actual)
 
     def test_decode_resp_to_message(self):
         resp_data = "*4\r\n$5\r\nHello\r\n:42\r\n$-1\r\n$5\r\nWorld\r\n"
         expected = ["Hello", 42, None, "World"]
         actual = decode_resp_to_message(resp_data)
-        self.assertEquals(expected, actual)
+        self.assertEqual(expected, actual)
