48974ca35e48abb5f83f474ac9b9057a8e00cab8
rojoyin <rojoyin@gmail.com>
2023-07-19 16:37:20 -0500

    Decode to bulk string

--- src/resp/deserializer.py
+++ src/resp/deserializer.py
@@ -7,3 +7,9 @@ def decode_to_simple_string(resp_data: str) -> str:
     encoded_pattern = rf"^\{EncodingHeader.SIMPLE_STRING}(.*){RESP_TRAILER}"
     match = re.search(encoded_pattern, resp_data)
     return match.group(1)
+
+
+def decode_to_bulk_string(resp_data: str) -> str | None:
+    encoded_pattern = rf"^\{EncodingHeader.BULK_STRING}([0-9]+){RESP_TRAILER}(.*){RESP_TRAILER}"
+    match = re.search(encoded_pattern, resp_data)
+    return match.group(2)
