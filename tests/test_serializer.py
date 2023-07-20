2a6b41cdb9d1f61850c96a1d90b63ed4fb48671b
rojoyin <rojoyin@gmail.com>
2023-07-20 00:27:03 -0500

    Update assert method

--- tests/test_serializer.py
+++ tests/test_serializer.py
@@ -9,7 +9,7 @@ class RESPTest(unittest.TestCase):
         message = "Hello, World!"
         actual = encode_simple_string(message)
         expected = "+Hello, World!\r\n"
-        self.assertEquals(actual, expected)
+        self.assertEqual(actual, expected)
 
     def test_is_simple_string(self):
         message = "Hello, World!"
@@ -28,7 +28,7 @@ class RESPTest(unittest.TestCase):
         message = 5
         actual = encode_int(message)
         expected = ":5\r\n"
-        self.assertEquals(actual, expected)
+        self.assertEqual(actual, expected)
 
     def test_error_no_int(self):
         message = 2.8
@@ -39,40 +39,40 @@ class RESPTest(unittest.TestCase):
         message = "bye"
         actual = encode_bulk_string(message)
         expected = "$3\r\nbye\r\n"
-        self.assertEquals(actual, expected)
+        self.assertEqual(actual, expected)
 
     def test_serialize_empty_bulk_string(self):
         message = ""
         actual = encode_bulk_string(message)
         expected = "$0\r\n\r\n"
-        self.assertEquals(actual, expected)
+        self.assertEqual(actual, expected)
 
     def test_serialize_errors(self):
         message = "An error occurred"
         actual = encode_error(message)
         expected = "-An error occurred\r\n"
-        self.assertEquals(actual, expected)
+        self.assertEqual(actual, expected)
 
     def test_serialize_null_bulk_string_values(self):
         message = None
         actual = encode_bulk_string(message)
         expected = "$-1\r\n"
-        self.assertEquals(actual, expected)
+        self.assertEqual(actual, expected)
 
     def test_serialize_null_array_values(self):
         message = [None]
         actual = encode_array(message)
         expected = "*-1\r\n"
-        self.assertEquals(actual, expected)
+        self.assertEqual(actual, expected)
 
     def test_serialize_array(self):
         message = ["Hello", 42, None, "World"]
         actual = encode_array(message)
         expected = "*4\r\n$5\r\nHello\r\n:42\r\n$-1\r\n$5\r\nWorld\r\n"
-        self.assertEquals(actual, expected)
+        self.assertEqual(actual, expected)
 
     def test_encode_message_to_resp(self):
         message = "holi"
         actual = encode_message_to_resp(message)
         expected = "$4\r\nholi\r\n"
-        self.assertEquals(actual, expected)
+        self.assertEqual(actual, expected)
