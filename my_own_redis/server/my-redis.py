dcddb09f50780818941840e1a2ee35db394aa906
rojoyin <rojoyin@gmail.com>
2025-04-26 16:31:28 -0500

    Add configuration to reuse socket with reduced timeout

--- my_own_redis/server/my-redis.py
+++ my_own_redis/server/my-redis.py
@@ -33,6 +33,7 @@ class MyRedis:
 
     def start(self):
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.server_socket.bind((self.host, self.port))
         self.server_socket.listen(5)
         print(f"Listening on {self.host}:{self.port}")
